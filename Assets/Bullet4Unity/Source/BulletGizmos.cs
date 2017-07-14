011e7f95c1ef3ae108490b6651dcaad1f5f08e31
Techgeek1 <austin.rife@gmail.com>
2017-07-14 00:23:31 -0700

    Added BulletGizmos for drawing visualizations Added drawing an approximation for convex hulls to BulletConvexHullShape

---
+++ Assets/Bullet4Unity/Source/BulletGizmos.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using BulletSharp;
+
+namespace Bullet4Unity {
+    public static class BulletGizmos {
+        public static void DrawConvexHull(ConvexHullShape shape, Mesh mesh, Transform transform, Color color, bool isDirty = false) {
+            if (shape == null)
+                return;
+
+            if (isDirty || mesh == null)
+                CreateHullGizmoMesh(shape, ref mesh);
+
+            Gizmos.color = color;
+            Gizmos.DrawWireMesh(mesh, transform.position, transform.rotation);
+        }
+
+        private static void CreateHullGizmoMesh(ConvexHullShape shape, ref Mesh mesh) {
+            if (mesh == null) {
+                mesh = new Mesh();
+            }
+            else {
+                mesh.Clear();
+            }
+
+            Vector3[] vertexBuffer;
+            int[] triangleBuffer;
+            using (ShapeHull hull = new ShapeHull(shape)) {
+                hull.BuildHull(0.0f);
+                vertexBuffer = hull.Vertices
+                    .Select(v => v.ToUnity())
+                    .ToArray();
+
+                triangleBuffer = hull.Indices
+                    .Select(i => (int)i)
+                    .ToArray();
+            }
+
+            MergeVertices(vertexBuffer, 0.05f);
+
+            mesh.vertices = vertexBuffer;
+            mesh.triangles = triangleBuffer;
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+        }
+
+        private static void MergeVertices(Vector3[] vertexBuffer, float tolerance) {
+            for (int i = 0; i < vertexBuffer.Length; i++) {
+                Vector3 v = vertexBuffer[i];
+                for (int j = i + 1; j < vertexBuffer.Length; j++) {
+                    if (Vector3.Distance(v, vertexBuffer[j]) < tolerance) {
+                        vertexBuffer[j] = v;
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Bullet4Unity/Source/CollisionShapes/BulletConvexHullShape.cs
+++ Assets/Bullet4Unity/Source/CollisionShapes/BulletConvexHullShape.cs
@@ -13,79 +13,74 @@ namespace Bullet4Unity {
     [AddComponentMenu("BulletPhysics/Collision/ConvexHullShape")]
     [RequireComponent(typeof(MeshFilter))]
     public class BulletConvexHullShape : BulletCollisionShape {
-        
+
         //Unity Inspector
         [Header("Shape Config")]
         [Tooltip("The mesh to use for convex hull generation")]
-        [SerializeField] private Mesh _hullMesh;
+        [SerializeField]
+        private Mesh _hullMesh;
         [Tooltip("Enable this to have Bullet automatically remove unnecessary geometry")]
-        [SerializeField] private bool _optimizeHull = true;
-        
+        [SerializeField]
+        private bool _optimizeHull = true;
+
         //Private Internal (Hull Generation)
         private List<BVector3> _meshVertices = new List<BVector3>();
         private List<Vector3> _hullVertices = new List<Vector3>();
-        
+
         //Generate and return a Bullet Convex Hull Shape
         private void GenerateConvexHull() {
             //Build the optimized hull
-            if (_hullMesh == null) _hullMesh = GetComponent<MeshFilter>().sharedMesh; 
+            if (_hullMesh == null)
+                _hullMesh = GetComponent<MeshFilter>().sharedMesh;
+
             _meshVertices = _hullMesh.vertices.Select(vertex => vertex.ToBullet()).ToList();
-             
+
             //Optimize the hull if optimization is enabled
             if (_optimizeHull) {
-                Shape = new ConvexHullShape(_meshVertices, 128) {LocalScaling = transform.localScale.ToBullet()};
+                Shape = new ConvexHullShape(_meshVertices, 128) { LocalScaling = transform.localScale.ToBullet() };
                 ((ConvexHullShape)Shape).OptimizeConvexHull();
             }
             else {
-                Shape = new ConvexHullShape(_meshVertices) {LocalScaling = transform.localScale.ToBullet()};
+                Shape = new ConvexHullShape(_meshVertices) { LocalScaling = transform.localScale.ToBullet() };
                 ((ConvexHullShape)Shape).OptimizeConvexHull();
                 if (((ConvexHullShape)Shape).NumPoints > 128) Debug.LogWarning("Convex hull contains more than 128 vertices!\n" +
                                                                 "Please consider enabling optimization or specifying an optimized hull mesh");
             }
-            
+
             //Fetch hull vertices for later use
-            ((ConvexHullShape) Shape).InitializePolyhedralFeatures();
-            for (var i = 0; i < ((ConvexHullShape) Shape).NumVertices; i++) {
+            ((ConvexHullShape)Shape).InitializePolyhedralFeatures();
+            for (var i = 0; i < ((ConvexHullShape)Shape).NumVertices; i++) {
                 BVector3 vertex;
-                ((ConvexHullShape) Shape).GetVertex(i, out vertex);
+                ((ConvexHullShape)Shape).GetVertex(i, out vertex);
                 _hullVertices.Add(vertex.ToUnity());
             }
-            
+
             //Cleanup
             _meshVertices.Clear();
         }
-        
-        #if UNITY_EDITOR
+
+#if UNITY_EDITOR
+        private Mesh _gizmoMesh = null;
+
         //Draw Shape Gizmo
         protected override void OnDrawGizmosSelected() {
-            //TODO: Visualize the actual hull, a bounding box is used for now
-            if (!DrawGizmo) return;
-            if (Shape == null) return;
-            Gizmos.color = GizmoColor;
+            if (!DrawGizmo)
+                return;
+
+            if (Shape == null && !Application.isPlaying)
+                GenerateConvexHull();
+
+            BulletGizmos.DrawConvexHull(Shape as ConvexHullShape, _gizmoMesh, transform, GizmoColor);
         }
-        #endif
-        
-        //Draw mesh using array of Vertices
-        private void DrawTrianglesVertices(Vector3[] vertices) {
-            if (vertices.Length == 0) return;
-            for (var i = 0; i + 3 < vertices.Length; i += 3) {
-                var a = vertices[i];
-                var b = vertices[i + 1];
-                var c = vertices[i + 2];
-                Debug.DrawLine(transform.position + a, transform.position + b, GizmoColor);
-                Debug.DrawLine(transform.position + b, transform.position + c, GizmoColor);
-                Debug.DrawLine(transform.position + c, transform.position + a, GizmoColor);
-            }
-        }
-        
-        
+#endif
+
         //Get Collision shape
         public override CollisionShape GetCollisionShape() {
             if (Shape != null) return Shape;
             GenerateConvexHull();
             return Shape;
-        } 
-        
+        }
+
         //Get Collision Shape Type
         public override CollisionShapeType GetShapeType() {
             return CollisionShapeType.ConvexHull;
